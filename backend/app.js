d0410f14d81300a7a96ea4a0e19a0f2b3557d28c
Nguyễn Chí Thân <137495065+chithan04@users.noreply.github.com>
2025-02-12 10:39:05 +0700

    config nodejs

--- backend/app.js
+++ backend/app.js
@@ -1,2 +1,20 @@
-const a = 113;
-console.log("🚀 ~ a:", a);
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+module.exports = app;
