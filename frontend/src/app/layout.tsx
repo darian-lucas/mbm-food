dffd66fba833b2012129ead18d147361471a5b5d
Huu Duc <leduc19122004@gmail.com>
2025-02-15 13:34:59 +0700

    Them

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from "next";
 import { manrope } from "@/utils";
 import "./globals.css";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import Introduce from "./introduce/introduce";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -21,7 +21,7 @@ export default function RootLayout({
         className={manrope.className}>
             <Header/>           
             <main>{children}</main>   
-            <Introduce></Introduce>
+            {/* <Introduce></Introduce> */}
             <Footer/>
       </body>
     </html>
