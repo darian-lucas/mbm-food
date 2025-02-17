31b75e52234f6921bd9751030fa1634acc2b1dc6
huynhduc2212 <lehuynhduc2279@gmail.com>
2025-02-17 11:31:47 +0700

    updtate 1

--- frontend/src/app/layout/ClientLayout.tsx
+++ frontend/src/app/layout/ClientLayout.tsx
@@ -1,7 +1,7 @@
 import { manrope } from "@/utils";
 // import "../globals.css";
-import Header from "../../components/Header";
-import Footer from "../../components/Footer";
+import Header from "../components/layout/Header";
+import Footer from "../components/layout/Footer";
 
 export default function ClientLayout({ children }: { children: React.ReactNode }) {
   return (
