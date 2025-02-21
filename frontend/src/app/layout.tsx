7981100654beb2e64c5ac85e4a694bc4f1bae1c3
Văn Sơn <hugiason16@gmail.com>
2025-02-21 20:19:24 +0700

    sr

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,15 +1,12 @@
 "use client";
 import { manrope } from "@/utils";
-import ClientLayout from "./layout/ClientLayout";
-import AdminLayout from "./layout/AdminLayout"
+import ClientLayout from "@/app/layout/ClientLayout";
+import AdminLayout from "@/app/layout/AdminLayout";
 import { usePathname } from "next/navigation";
 
 import "./globals.css";
-import ClientLayout from "@/app/layout/ClientLayout";
-import AdminLayout from "@/app/layout/AdminLayout"; // Import AdminLayout
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-
 
 export default function RootLayout({
   children,
@@ -22,7 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={manrope.className}>
-      <ToastContainer position="top-right" autoClose={3000} />
+        <ToastContainer position="top-right" autoClose={3000} />
         {isAdmin ? <AdminLayout>{children}</AdminLayout> : <ClientLayout>{children}</ClientLayout>}
       </body>
     </html>
