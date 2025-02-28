d082715ea4f807d5bcbf84efe1e3c6fca16ec6a5
Văn Sơn <hugiason16@gmail.com>
2025-02-28 21:30:43 +0700

    api cmt, Ctrl z 1 số hàm user bị Nhân vật H hủy

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,7 @@ const categoryRoutes = require('./src/routes/category');
 const productRoutes = require('./src/routes/product');
 const postRoutes = require('./src/routes/post')
 const favoriteRoutes = require('./src/routes/favorite');
-
+const postCommentRoutes = require('./src/routes/postComment');
 
 const cors = require("cors");
 const compression = require("compression");
@@ -37,7 +37,7 @@ app.use('/api', postRoutes);
 app.use('/api/categories', categoryRoutes);
 app.use('/api/products', productRoutes);
 app.use('/api/favorite', favoriteRoutes);
-
+app.use('/api/cmt', postCommentRoutes);
 
 app.listen(PORT, () => {
     console.log(`🚀 Server chạy tại: http://localhost:${PORT}`);
