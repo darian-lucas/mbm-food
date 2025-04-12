d4e911fff1a9f088e90336e30093348f861c91d5
Huu Duc <leduc19122004@gmail.com>
2025-04-12 13:29:14 +0700

    Update tin tuc

--- frontend/src/app/news/page.tsx
+++ frontend/src/app/news/page.tsx
@@ -115,11 +115,11 @@ export default function New() {
                   </li>
                   {isOpen && (
                     <ul className="submenu">
-                      <li><Link href="/product/pizza">Pizza</Link></li>
-                      <li><Link href="/products/khaivi">Khai vị</Link></li>
-                      <li><Link href="/products/myy">Mỳ Ý</Link></li>
-                      <li><Link href="/products/salad">Salad</Link></li>
-                      <li><Link href="/products/thucuong">Thức uống</Link></li>
+                      <li><Link href="/pizza">Pizza</Link></li>
+                      <li><Link href="/khai-vi">Khai vị</Link></li>
+                      <li><Link href="/my-y">Mỳ Ý</Link></li>
+                      <li><Link href="/salad">Salad</Link></li>
+                      <li><Link href="/thuc-uong">Thức uống</Link></li>
                     </ul>
                   )}
                   <li><Link className="font-bold" href="/news">Tin tức</Link></li>
